426862f605ba75952a0892e9d021f32177ca0300
liuzhen <lzudx@foxmail.com>
2021-07-21 10:37:16 +0800

    初始版本提交  verison 1.2

---
+++ src/main/java/comm/study/bean/Image.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright(c) 2018 asura
+ */
+package comm.study.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p></p>
+ *
+ *
+ * @Description:
+ * @ClassName Image
+ * @Author zhen.liu
+ * @Date 2021/7/21 10:19 上午
+ * @Version 1.0
+ **/
+@Getter
+@Setter
+public class Image {
+
+    private String url;
+
+    private Long width;
+
+    private Long height;
+}
